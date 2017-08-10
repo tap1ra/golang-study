ac54aeea24ab47f1d1b77be0e086e4ccffce3976
tahira <tahira@pepabo.com>
2017-08-10 23:23:53 +0900

    add digest authorize

---
+++ digest-authorize.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/k0kubun/pp"
+)
+
+func handlerDigest(w http.ResponseWriter, r *http.Request) {
+	pp.Printf("URL :%s\n", r.URL.String())
+	pp.Printf("Query: %v\n", r.URL.Query())
+	pp.Printf("Proto: %s\n", r.Proto)
+	pp.Printf("Method: %s\n", r.Method)
+	pp.Printf("Header: %v\n", r.Header)
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("--body--\n%s\n", string(body))
+	if _, ok := r.Header["Authorization"]; !ok {
+		w.Header().Add("WWW-Authenticate", `Digest realm="Secret Zone", nonce="hoge-fuga", algorithm-MD5, qop="auth"`)
+		w.WriteHeader(http.StatusUnauthorized)
+	} else {
+		fmt.Fprintf(w, "<html><body>secret page</body></html>\n")
+	}
+
+}
+
+func main() {
+	var httpServer http.Server
+	http.HandleFunc("/digest", handlerDigest)
+	log.Println("start http listening :18888")
+	httpServer.Addr = ":18888"
+	log.Println(httpServer.ListenAndServe())
+}
